1d191e95f59d0cd5e8884819616438514809d5bc
Ayan Sengupta <ayan.sengupta27@gmail.com>
2018-11-13 19:51:00 -0500

    first commit

---
+++ routes/api/profile.js
@@ -0,0 +1,10 @@
+const express = require("express");
+
+const router = express.Router();
+
+//@route        GET api/profile/test
+//@description  tests profile routes
+//@access       public
+router.get("/test", (req, res) => res.json({ msg: "Profile works" }));
+
+module.exports = router;
--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const passport = require("passport");
 
+const users = require("./routes/api/users");
+const profile = require("./routes/api/profile");
+const posts = require("./routes/api/posts");
+
 //creating app
 const app = express();
 
@@ -14,6 +18,11 @@ mongoose
   .connect(db)
   .then(() => console.log("Mongodb connected"))
   .catch(err => console.log("DB connection error"));
+
+//configuring routes
+app.use("/api/users", users);
+app.use("/api/profile", profile);
+app.use("/api/posts", posts);
 
 //declaring port
 const port = process.env.PORT || 3000;
