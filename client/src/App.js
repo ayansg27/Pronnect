715b24b11d27c95d279df3b11e25c87607c3045b
Ayan Sengupta <ayan.sengupta27@gmail.com>
2018-11-20 16:26:53 -0500

    login and register component

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,31 @@
+//import packages
 import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
+//import components
 import Navbar from "./components/layout/Navbar";
 import Footer from "./components/layout/Footer";
 import Landing from "./components/layout/Landing";
+import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
+
+//import stylesheet
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Navbar />
-        <Landing />
-        <Footer />
-      </div>
+      <Router>
+        <div className="App">
+          <Navbar />
+          <Route exact path="/" component={Landing} />
+          <div className="container">
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/login" component={Login} />
+          </div>
+          <Footer />
+        </div>
+      </Router>
     );
   }
 }
