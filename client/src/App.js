0e66f73a3157418947d5167ccbb73b21288f3cfc
Ayan Sengupta <ayan.sengupta27@gmail.com>
2018-11-20 20:33:22 -0500

    redux and store setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 //import packages
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Provider } from "react-redux";
 
 //import components
 import Navbar from "./components/layout/Navbar";
@@ -8,6 +9,7 @@ import Footer from "./components/layout/Footer";
 import Landing from "./components/layout/Landing";
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
+import store from "./store";
 
 //import stylesheet
 import "./App.css";
@@ -15,17 +17,19 @@ import "./App.css";
 class App extends Component {
   render() {
     return (
-      <Router>
-        <div className="App">
-          <Navbar />
-          <Route exact path="/" component={Landing} />
-          <div className="container">
-            <Route exact path="/register" component={Register} />
-            <Route exact path="/login" component={Login} />
+      <Provider store={store}>
+        <Router>
+          <div className="App">
+            <Navbar />
+            <Route exact path="/" component={Landing} />
+            <div className="container">
+              <Route exact path="/register" component={Register} />
+              <Route exact path="/login" component={Login} />
+            </div>
+            <Footer />
           </div>
-          <Footer />
-        </div>
-      </Router>
+        </Router>
+      </Provider>
     );
   }
 }
